fb935e38f7450d9e08f40e89637a068f0b33fcab
c-bruce <callum.bruce1@gmail.com>
2025-05-16 19:32:34 +0100

    Receiving SensorData

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,46 @@
 #include <Arduino.h>
 
-const int ledPin = 13; // the number of the LED pin
-char incomingByte; // for incoming serial data
+// const int ledPin = 13; // the number of the LED pin
+// char incomingByte; // for incoming serial data
+
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(ledPin, OUTPUT); // initialize the LED pin as an output
+// }
+
+// void loop() {
+//   if (Serial.available() > 0) {
+//     Serial.println("Serial.available() > 0");
+//     incomingByte = Serial.read();
+//     if (incomingByte == 'H') {
+//       Serial.print('H');
+//       Serial.println(incomingByte);
+//       digitalWrite(ledPin, HIGH); // turn the LED on
+//     } else if (incomingByte == 'L') {
+//       Serial.print('L');
+//       Serial.println(incomingByte);
+//       digitalWrite(ledPin, LOW); // turn the LED off
+//     }
+//   }
+// }
+
+struct SensorData {
+  int temperature;
+  float voltage;
+};
+
+SensorData receivedData;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT); // initialize the LED pin as an output
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    Serial.println("Serial.available() > 0");
-    incomingByte = Serial.read();
-    if (incomingByte == 'H') {
-      Serial.print('H');
-      Serial.println(incomingByte);
-      digitalWrite(ledPin, HIGH); // turn the LED on
-    } else if (incomingByte == 'L') {
-      Serial.print('L');
-      Serial.println(incomingByte);
-      digitalWrite(ledPin, LOW); // turn the LED off
-    }
+  if (Serial.available() >= sizeof(SensorData)) {
+    Serial.readBytes((uint8_t *)&receivedData, sizeof(SensorData));
+    Serial.print("Temperature: ");
+    Serial.print(receivedData.temperature);
+    Serial.print(" | Voltage: ");
+    Serial.println(receivedData.voltage);
   }
 }
